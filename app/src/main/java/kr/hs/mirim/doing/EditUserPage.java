cbcfec09cb965e85ed51c20376944934057fa11a
SeoeunPark <s2019w29@e-mirim.hs.kr>
2021-04-06 23:19:53 +0900

    [수정] EditUserPage.java activity_edit_user_page.xml
    -회원정보 수정

--- app/src/main/java/kr/hs/mirim/doing/EditUserPage.java
+++ app/src/main/java/kr/hs/mirim/doing/EditUserPage.java
@@ -1,7 +1,9 @@
 package kr.hs.mirim.doing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -13,26 +15,61 @@ import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
 
 public class EditUserPage extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     private ImageView edit_profile;
     private TextView edit_nickname;
     private EditText edit_nickname_nim;
     private EditText edit_I_doing;
+    private EditText edit_about;
+    private EditText edit_level;
+    private EditText edit_condition;
     private Switch edit_onoff_direct;
+    private Button edit_save;
+
+    private String user_about = ""; //about
+    private String user_id=""; //id
+    private String user_email = ""; //이메일
+    private int user_condition = 0; //1~8까지의
+    private String user_ing = ""; //하는 중
+    private int user_level = 0; // 단계
+    private String user_name = ""; //유저네임
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_page);
-        edit_profile = (ImageView) findViewById(R.id.edit_profile_circle);
-        edit_nickname = (TextView) findViewById(R.id.edit_nickname);
-        edit_nickname_nim = (EditText) findViewById(R.id.edit_nickname_nim);
-        edit_I_doing = (EditText) findViewById(R.id.edit_I_doing);
-        edit_onoff_direct = (Switch) findViewById(R.id.edit_onoff_direct);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        edit_profile = (ImageView) findViewById(R.id.edit_profile_circle); // 프로필 상태
+        edit_nickname_nim = (EditText) findViewById(R.id.edit_nickname_nim); // 님은
+        edit_onoff_direct = (Switch) findViewById(R.id.edit_onoff_direct); // 쪽지 허용 비허용
+        edit_save =(Button)findViewById(R.id.edit_save); //저장버튼
+
+        edit_about = (EditText) findViewById(R.id.edit_about); // 한줄소개
+        edit_condition = (EditText)findViewById(R.id.edit_condition); //1~3
+        edit_I_doing = (EditText) findViewById(R.id.edit_I_doing); //~중
+        edit_level = (EditText) findViewById(R.id.edit_level); //1,2,3
+        edit_nickname = (TextView) findViewById(R.id.edit_nickname); // 이름
+
 
         //이름수정
         edit_nickname.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +115,36 @@ public class EditUserPage extends AppCompatActivity {
                 alertDialog.show();
             }
         });
+        edit_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditUser();
+            }
+        });
+        }
+        private void EditUser(){
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            FirebaseUser user = mAuth.getCurrentUser();
+            user_id = user.getUid();
+            user_email = user.getEmail();
 
+            HashMap<String, Object> userMap = new HashMap<>();
+            userMap.put("about",edit_about.getText().toString());
+            userMap.put("condition",2);
+            userMap.put("ing",edit_I_doing.getText().toString());
+            userMap.put("level",1);
+            userMap.put("name",edit_nickname.getText().toString());
+
+            db.collection("User").document(user_id).set(userMap, SetOptions.merge()).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(main);
+                    }else {
+                        String error = task.getException().getMessage();
+                    }
+                }
+            });
     }
 }
