c5bfb20dd2a08407a689c7cd917383039fc729bf
s2019w29 <s2019w29@e-mirim.hs.kr>
2021-05-22 15:37:43 +0900

    FriendsList.java 리스트 수정

--- app/src/main/java/kr/hs/mirim/doing/FriendsList.java
+++ app/src/main/java/kr/hs/mirim/doing/FriendsList.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,10 +26,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -38,6 +41,8 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -61,7 +66,7 @@ public class FriendsList extends Fragment {
     private DatabaseReference databaseReference1;
     private DatabaseReference databaseReference2;
 
-    private int alfriend=0;
+    private int alfriend = 0;
 
     //인디케이터 만들기
     // newInstance constructor for creating fragment with arguments
@@ -97,59 +102,92 @@ public class FriendsList extends Fragment {
         FirebaseUser user = auth.getCurrentUser();
         user_id = user.getUid();
 
+
         database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
         databaseReference1 = database.getReference("my_friends").child(user_id); // DB 테이블 연결
         databaseReference2 = database.getReference("users"); // DB 테이블 연결
 
+        //version1
+//        databaseReference2.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+//                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {// 반복문으로 데이터 List를 추출해냄
+//                    String key = snapshot.getKey();
+//                    databaseReference1.addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(DataSnapshot dataSnapshot2) {
+//                            for (DataSnapshot snapshot2 : dataSnapshot2.getChildren()) {// 반복문으로 데이터 List를 추출해냄
+//                                String key2 = (String) snapshot2.child("code").getValue();
+//                                if (key.equals(key2)) {
+//                                    MyFriendList MyFriendList = snapshot.getValue(MyFriendList.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+//                                    arrayList.add(MyFriendList); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준다
+//                                }
+//                                continue;
+//                            }
+//                            adapter.notifyDataSetChanged();
+//                        }
+//                        @Override
+//                        public void onCancelled(DatabaseError databaseError) {
+//                        }
+//                    });
+//                    }
+//                adapter = new FriendAdapter(arrayList, getContext());
+//                recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                // 디비를 가져오던중 에러 발생 시
+//                Log.e("Fraglike", String.valueOf(databaseError.toException())); // 에러문 출력
+//            }
+//        });
+        
 
-        databaseReference2.addValueEventListener(new ValueEventListener() {
+        databaseReference1.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
-                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {// 반복문으로 데이터 List를 추출해냄
-                    String key = snapshot.getKey();
-
-                    databaseReference1.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot2) {
+                arrayList.clear();
+                for (DataSnapshot snapshot2 : dataSnapshot2.getChildren()) {// 반복문으로 데이터 List를 추출해냄
+                    String key2 = (String) snapshot2.child("code").getValue();
+                    databaseReference2.addValueEventListener(new ValueEventListener() {
                         @Override
-                        public void onDataChange(DataSnapshot dataSnapshot2) {
-                            for (DataSnapshot snapshot2 : dataSnapshot2.getChildren()) {// 반복문으로 데이터 List를 추출해냄
-                                String key2 = (String) snapshot2.child("code").getValue();
-                                if(key.equals(key2)) {
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {// 반복문으로 데이터 List를 추출해냄
+                                String key = snapshot.getKey();
+                                if (key.equals(key2)) {
                                     MyFriendList MyFriendList = snapshot.getValue(MyFriendList.class); // 만들어뒀던 User 객체에 데이터를 담는다.
                                     arrayList.add(MyFriendList); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준다
                                 }
+
                             }
                             adapter.notifyDataSetChanged();
+
                         }
 
                         @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // 디비를 가져오던중 에러 발생 시
+                            Log.e("Fraglike", String.valueOf(databaseError.toException())); // 에러문 출력
                         }
+
                     });
-
-
-
-//                    MyFriendList MyFriendList = snapshot.getValue(MyFriendList.class); // 만들어뒀던 User 객체에 데이터를 담는다.
-//                    arrayList.add(MyFriendList); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준다
-
-
                 }
-                adapter.notifyDataSetChanged();
-//                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
+                adapter = new FriendAdapter(arrayList, getContext());
+                recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // 디비를 가져오던중 에러 발생 시
+            public void onCancelled(DatabaseError databaseError) {
                 Log.e("Fraglike", String.valueOf(databaseError.toException())); // 에러문 출력
             }
         });
-        adapter = new FriendAdapter(arrayList, getContext());
-        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
 
 
+        //친구 추가하기
         add_friend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -162,7 +200,7 @@ public class FriendsList extends Fragment {
                 dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        alfriend=0;
+                        alfriend = 0;
                         String username = inputname.getText().toString();
                         if (!TextUtils.isEmpty(username)) {
                             FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -183,27 +221,25 @@ public class FriendsList extends Fragment {
                                                     public void onDataChange(DataSnapshot dataSnapshot2) {
                                                         for (DataSnapshot snapshot2 : dataSnapshot2.getChildren()) {// 반복문으로 데이터 List를 추출해냄
                                                             String key2 = (String) snapshot2.child("code").getValue();
-                                                            if(alfriend!=2) {
+                                                            if (alfriend != 2) {
                                                                 if (document.getId().equals(key2)) {
                                                                     Toast.makeText(getActivity(), "이미 등록된 친구입니다.", Toast.LENGTH_SHORT).show();
                                                                     alfriend = 1;
                                                                 }
                                                             }
                                                         }
-                                                        if(alfriend==0) {
+                                                        if (alfriend == 0) {
                                                             my_friends.put("code", document.getId());
                                                             FirebaseDatabase.getInstance().getReference().child("my_friends").child(user_id).push().setValue(my_friends);
                                                             Toast.makeText(getActivity(), "추가되었습니다.", Toast.LENGTH_SHORT).show();
-                                                            alfriend=2;
+                                                            alfriend = 2;
                                                         }
                                                     }
 
                                                     @Override
                                                     public void onCancelled(DatabaseError databaseError) {
-
                                                     }
                                                 });
-
                                             }
                                         });
                                     }
