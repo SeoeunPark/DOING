d73d5ed7ba8aec94fd994943dd99020502695256
KimAra <s2019w06@e-mirim.hs.kr>
2021-05-28 07:40:15 +0900

    add sendPost

--- app/src/main/java/kr/hs/mirim/doing/sendPost.java
+++ app/src/main/java/kr/hs/mirim/doing/sendPost.java
@@ -3,12 +3,26 @@ package kr.hs.mirim.doing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class sendPost extends AppCompatActivity {
-
+public class sendPost extends AppCompatActivity implements View.OnClickListener{
+    Button[] customStringBtn;
+    String[] customString = new String[5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_post);
+        customStringBtn = new Button[]{findViewById(R.id.custom_message1), findViewById(R.id.custom_message2), findViewById(R.id.custom_message3), findViewById(R.id.custom_message4), findViewById(R.id.custom_message5)};
+
+
+    }
+
+    @Override
+    public void onClick(View view){
+        findViewById(view.getId()).setPressed(true);
+        if(view.getId() == R.id.sendBtn){
+
+        }
     }
 }
